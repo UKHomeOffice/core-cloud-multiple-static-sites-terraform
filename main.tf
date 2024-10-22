718fd005951a0c9acc63e840d2b0c2c51886a1af
Rebecca Whitehurst-Martin <Rebecca.Whitehurst-Martin@digital.homeoffice.gov.uk>
2024-10-22 14:48:46 +0100

    initial module commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "static_site" {
+  source = "git::https://github.com/UKHomeOffice/core-cloud-static-site-terraform.git?ref=0.0.1"
+
+  for_each = var.sites
+
+  cloud_front_default_vars = var.cloud_front_default_vars
+  aws_region               = var.aws_region
+  tenant_vars              = var.tenant_vars.each.value
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "tenant_vars" {
+  type = any
+}
+
+variable "cloud_front_default_vars" {
+  type = any
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "sites" {
+  type = list(string)
+}
