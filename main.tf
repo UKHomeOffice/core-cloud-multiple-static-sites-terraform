b515f4c0a0f10234e9ae20a6fb62b9166450effb
Rebecca Whitehurst-Martin <Rebecca.Whitehurst-Martin@digital.homeoffice.gov.uk>
2024-10-23 16:56:37 +0100

    move attempt to use fileset to perform iteration into terragrunt

--- main.tf
+++ main.tf
@@ -6,14 +6,11 @@ provider "aws" {
 module "static_site" {
   source = "git::https://github.com/UKHomeOffice/core-cloud-static-site-terraform.git?ref=CCL-499-c"
 
-#  for_each = var.tenant_vars
-  for_each = fileset(path.module, "./*/config.yaml")
-
+  for_each = var.tenant_vars
 
   cloud_front_default_vars = var.cloud_front_default_vars
   aws_region               = var.aws_region
-#  tenant_vars              = each.value
-  tenant_vars              = yamldecode(file("each.value"))
+  tenant_vars              = each.value
 
   providers = {
     aws.us-east-1 = aws.us-east-1
