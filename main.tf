4b6456e01436704ab1dd965a5a684d990c94be1a
Iqbal Shaikh <iqbal.shaikh2@digital.homeoffice.gov.uk>
2024-12-19 16:44:33 +0000

    Updating to latest WAF version

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "static_site" {
-  source = "git::https://github.com/UKHomeOffice/core-cloud-static-site-terraform.git?ref=0.0.4"
+  source = "git::https://github.com/UKHomeOffice/core-cloud-static-site-terraform.git?ref=0.0.6"
 
   for_each = var.tenant_vars
 
