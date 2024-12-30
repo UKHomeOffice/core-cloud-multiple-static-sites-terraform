52edc86408526009daff43cc079feb1d507bb271
Iqbal Shaikh <iqbal.shaikh2@digital.homeoffice.gov.uk>
2024-12-30 15:43:40 +0000

    Removing count as its overriding the default rules

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "static_site" {
-  source = "git::https://github.com/UKHomeOffice/core-cloud-static-site-terraform.git?ref=0.0.6"
+  source = "git::https://github.com/UKHomeOffice/core-cloud-static-site-terraform.git?ref=0.0.7"
 
   for_each = var.tenant_vars
 
