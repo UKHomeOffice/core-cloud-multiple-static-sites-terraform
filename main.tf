b3eca90b7a460e966155f0209650f794698d3f7f
Rebecca Whitehurst-Martin <Rebecca.Whitehurst-Martin@digital.homeoffice.gov.uk>
2025-02-05 17:01:36 +0000

    bump ver

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "static_site" {
-  source = "git::https://github.com/UKHomeOffice/core-cloud-static-site-terraform.git?ref=0.1.3"
+  source = "git::https://github.com/UKHomeOffice/core-cloud-static-site-terraform.git?ref=0.1.4"
 
   for_each = var.tenant_vars
 
