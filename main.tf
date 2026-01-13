b0e4e58230698aceb5d87d74c0311c8049948215
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2026-01-13 10:08:39 +0000

    reverting some changes for testing

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
