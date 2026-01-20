a365cff4d7f4c9d43497256b82158ee391525491
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2026-01-20 13:49:38 +0000

    change following PR comment

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "s3_bucket_name" {
-  value       = module.static_site["corecloud_staticsite_terratest"].s3_bucket_name
+  value       = module.static_site.s3_bucket_name
 }
 
 output "cloudfront_distribution_domain_name" {
-  value       = module.static_site["corecloud_staticsite_terratest"].cloudfront_distribution_domain_name
+  value       = module.static_site.cloudfront_distribution_domain_name
 }
