6c27fefffed2673a4c025715db7056c4564c2341
Elliott Hrynacz <elliott.hrynacz@digital.homeoffice.gov.uk>
2026-01-20 13:56:46 +0000

    key module mapping

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,13 @@
 output "s3_bucket_name" {
-  value       = module.static_site.s3_bucket_name
+  value = {
+    for key, module in module.static_site :
+    key => module.s3_bucket_name
+  }
 }
 
 output "cloudfront_distribution_domain_name" {
-  value       = module.static_site.cloudfront_distribution_domain_name
+  value = {
+    for key, module in module.static_site :
+    key => module.cloudfront_distribution_domain_name
+  }
 }
